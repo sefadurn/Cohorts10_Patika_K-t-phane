bbc9c518bd412e45e6954366259d150269feea05
user <user@DESKTOP-U6UHBUC>
2025-07-13 12:38:30 +0300

    Proje dosyası ekle.

---
+++ Week4_PatikaKutuphane/Kutuphane.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4_PatikaKutuphane
+{
+    public class Kutuphane
+    {
+
+        // Property'leri tanımlayalım.
+
+        public string Ad { get; set; }
+        public string YazarAdı { get; set; }
+        public int SayfaSayisi { get; set; }
+        public string Yayinevi { get; set; }
+        public DateTime KayitTarihi { get; set; }
+
+        public Kutuphane()      // Default Constructor
+        {
+            KayitTarihi =DateTime.Now;
+        }
+
+       
+        
+        public Kutuphane(string ad, string yazarAdi, int sayfaSayisi, string yayinEvi)
+        {
+            Ad = ad;                // ÖNEMLİ NOT: CONSTRUCTOR İÇİNDE FİELD DEĞİL PROPERTY KULLAN !!!
+            YazarAdı = yazarAdi;
+            SayfaSayisi = sayfaSayisi;
+            Yayinevi = yayinEvi;
+            KayitTarihi =DateTime.Now;
+        }
+        public void Yazdir()        // Konsola yazdırmak için metot tanımladık.
+        {
+            Console.WriteLine($"Bu kitabın Adı: {Ad} Yazarı: {YazarAdı} Sayfa sayisi: {SayfaSayisi} Yayinevi: {Yayinevi} Kayıt Tarihi: {KayitTarihi.ToShortDateString()}");
+        }
+    }
+}
---
+++ Week4_PatikaKutuphane/Program.cs
@@ -0,0 +1,18 @@
+﻿using Week4_PatikaKutuphane;
+
+//Parametreli Constructor için;
+Kutuphane Kitap1 = new Kutuphane("Adı Aylin", "Ayşe Kulin", 398,"Remzi Kitabevi");
+
+//Default Constructor için;
+Kutuphane Kitap2 = new Kutuphane();
+Kitap2.Ad = "Adı Aylin";
+Kitap2.YazarAdı = "Ayşe Kulin";
+Kitap2.SayfaSayisi = 398;
+Kitap2.Yayinevi = "Remzi Kitabevi";
+
+
+
+Console.WriteLine("Parametreli Constructor ile oluşturulan kitap:");
+Kitap1.Yazdir();
+Console.WriteLine("Default Constructor ile oluşturulan kitap:");
+Kitap2.Yazdir();
