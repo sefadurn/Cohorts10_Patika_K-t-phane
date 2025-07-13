d4a55203ae711a2abc26de00251b5268037ff9b8
Sefa <sefadurn@icloud.com>
2025-07-13 12:52:48 +0300

    Program.cs

--- Week4_PatikaKutuphane/Program.cs
+++ Week4_PatikaKutuphane/Program.cs
@@ -16,3 +16,16 @@ Console.WriteLine("Parametreli Constructor ile oluşturulan kitap:");
 Kitap1.Yazdir();
 Console.WriteLine("Default Constructor ile oluşturulan kitap:");
 Kitap2.Yazdir();
+
+// Kodlarınızın altına birer yorum satırıyla örnek üzerinden Class , Property , New , Constructor kavramlarını açıklayınız.
+
+// Class --> "Kutuphane" adında bir sınıf tanımlıyoruz.
+// Sınıf (Class), bir nesnenin şablonudur. Kitap nesnelerinin özelliklerini ve davranışlarını tanımlar.
+
+// Property: Her biri Kitap nesnesine ait bir özelliği tanımlar. Bizim örnekte; Ad, YazarAdı, SayfaSayisi gibi..
+// Property'ler veri saklar ve dışarıdan erişimi sağlar
+
+// new: "Kutuphane" sınıfından bir nesne (örnek) oluşturuyoruz.
+// "new" anahtar kelimesi, bellekte yeni bir Kitap1 nesnesi yaratır.
+
+// Constructor (Yapıcı Metot): Nesne oluşturulurken çalışır. Parametreli ve parametresiz (default) türleri vardır.
